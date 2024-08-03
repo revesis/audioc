7a5c42301e564f9d99211f0dba7452e2a5adeb13
alcyonides <3295857282@qq.com>
2024-08-03 10:13:04 +0800

    fix logic error

--- build/build.sh
+++ build/build.sh
@@ -14,16 +14,35 @@ echo "Preprocessing End"
 
 echo "Compilation Start"
 
-uglifyjs a.i -o audioc.min.js -cm
-uglifyjs a.i -o audioc-ie8.min.js -cm --ie8
+uglifyjs a.i -o a.s -cm
+uglifyjs a.i -o a-ie8.s -cm --ie8
 
-# cat << EOF > ./audioc.min.js
-# EOF
+# uglifyjs a.i -o audioc.min.js -cm
+# uglifyjs a.i -o audioc-ie8.min.js -cm --ie8
+cat << EOF > ./a.o
+/*!
+ * Audioc Player v0.1.6
+ */
+EOF
+cat << EOF > ./a-ie8.o
+/*!
+ * Audioc Player v0.1.6
+ */
+EOF
+
+cat a.s >> a.o
+cat a-ie8.s >> a-ie8.o
+
+mv a.o audioc.min.js
+mv a-ie8.o audioc-ie8.min.js
 
 echo "Compilation End"
 
 echo "Clear Start"
 
 rm -f ./a.i
+rm -f ./a.s ./a-ie8.s
+rm -f ./a.o ./a-ie8.o
+rm -f ./a.ie
 
 echo "Clear End"
